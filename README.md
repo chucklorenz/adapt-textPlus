2b51d9f32631661de63d3f3ea3a6aaa2f15e2d33
Chuck F. Lorenz <chucklorenz@yahoo.com>
2014-11-11 10:56:22 -0600

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,79 @@
+#adapt-textPlus
+
+A presentation component based on the adapt core contributed text component, with two modifications: an optional graphic and the ability to use popups to present lengthy content when device screensize is small.
+
+
+##Installation
+
+First, be sure to install the [Adapt Command Line Interface](https://github.com/adaptlearning/adapt-cli), then from the command line run:-
+
+        adapt install adapt-textPlus
+
+This component can also be installed by adding the component to the adapt.json file before running `adapt install`:
+
+        "adapt-textPlus": "*"
+
+##Usage
+
+This is a presentation component with optional graphic, displayTitle, body, and instruction elements. The graphic element allows for optimization for various devices, swapping out images based on screen size. (`screenSize`--small, medium, and large--is specified in theme.json.)
+
+##Settings overview
+
+An complete example of this components settings can be found in the [example.json](placeholder) file. A description of the core settings can be found at: [Core model attributes](https://github.com/adaptlearning/adapt_framework/wiki/Core-model-attributes)
+
+####_component
+
+This value must be: `textPlus`
+
+####_classes
+
+You can use this setting to add custom classes to your template and LESS file.
+
+####_layout
+
+This defines the position of the component in the block. Values can be `full`, `left` or `right`. 
+
+####displayTitle, body, instruction
+
+The `_graphic`,`displayTitle`, `body`, and `instruction` settings can be left blank. However, in this case the blank component would be better suited: [adapt-contrib-blank](https://github.com/adaptlearning/adapt-contrib-blank)
+
+####_graphic
+
+The image for this component is defined within this element. The `_graphic` element should contain only one value for `alt`, `title`, `large`, `medium` and `small`. The `_graphic` element can be used without `displayTitle`, `body`, and `instruction`. However, in this case the graphic component would be better suited: [adapt-contrib-graphic](https://github.com/adaptlearning/adapt-contrib-graphic).
+
+####alt
+
+The `alt` setting provides alternative information for the image.
+
+####title
+
+This setting is for the title attribute on the image.
+
+####large
+
+Enter a path to the image for large device width. Paths should be relative to the src folder, e.g., course/en/images/pic8_lg.jpg.
+
+####medium
+
+Enter a path to the image for medium device width. Paths should be relative to the src folder, e.g., course/en/images/pic8_md.jpg.
+
+####small
+
+Enter a path to the image for small device width. Paths should be relative to the src folder, e.g., course/en/images/pic8_sm.jpg.
+
+####_maxBodyCharacters
+
+When device.screenSize equals small (e.g., smartphones), this setting specifies the number of characters of the body content that will be displayed. If the body content exceeds this number, an ellipsis will replace the excess and it will be followed by a link to a popup of the complete body content. If not specified, `_maxBodyCharacters` defaults to 100. To prevent the use of popups, set `_maxBodyCharacters` to a number greater than the body's character count.
+
+####moreText
+
+A word, phrase, or other representation in characters that indicate that more content will be displayed in a popup by clicking this link. If not specified, `moreText` defaults to `More`.
+
+##Limitations
+
+To be completed.
+
+##Browser spec
+
+To be completed.
+
---
+++ js/adapt-textPlus.js
@@ -0,0 +1,122 @@
+/*
+* adapt-textPlus
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Code is based on adapt-contrib-text, adapt-contrib-graphic, and adapt-contrib-reveal.
+*/
+define(function(require) {
+
+    var ComponentView = require('coreViews/componentView');
+    var Adapt = require('coreJS/adapt');
+
+    var TextPlus = ComponentView.extend({
+
+        events: function () {
+            return Adapt.device.touch == true ? {
+                'inview' : 'inview',
+                'touchstart .textPlus-popup-open' : 'openPopup',
+                'click .textPlus-popup-open' : 'openPopup',
+                'touchstart .textPlus-popup-close' : 'closePopup',
+                'click .textPlus-popup-close' : 'closePopup'
+            }:{
+                'inview' : 'inview',
+                'click .textPlus-popup-open' : 'openPopup',
+                'click .textPlus-popup-close' : 'closePopup'
+            }
+        },
+
+        preRender: function () {
+            this.listenTo(Adapt, 'device:changed', this.setDeviceSize, this);
+            this.listenTo(Adapt, 'device:changed', this.resizeImage);
+            this.listenTo(Adapt, 'device:resize', this.setDeviceSize, this);
+            this.setDeviceSize();
+        },
+
+        setDeviceSize: function() {
+            if (Adapt.device.screenSize === 'small') {
+                var charLimit = 100;
+                var firstCharLimit = this.model.get('_maxBodyCharacters') || charLimit;
+                var hasPopup = this.model.get('body') && this.model.get('body').length > firstCharLimit;
+                if (hasPopup) {
+                    this.model.set('body-short', this.model.get('body').substring(0, firstCharLimit) + '...');
+                }
+                this.model.set('_displayShortText', hasPopup);
+                if (hasPopup) {
+                    this.$('.textPlus-short').removeClass('textPlus-hidden');
+                    this.$('.textPlus-long').addClass('textPlus-hidden');
+                }
+                return;
+            }
+            this.model.set('_displayShortText', false);
+            this.$('.textPlus-short').addClass('textPlus-hidden');
+            this.$('.textPlus-long').removeClass('textPlus-hidden');
+        },
+
+        postRender: function() {
+            if(this.model.has('_graphic')){
+                this.resizeImage(Adapt.device.screenSize);
+            } else {
+                this.setReadyStatus();
+            }
+            // Check if instruction or body is set, otherwise force completion
+            var cssSelector = this.$('.component-instruction').length > 0 ? '.component-instruction' 
+                : (this.$('.component-body').length > 0 ? '.component-body' : null);
+
+            if (!cssSelector) {
+                this.setCompletionStatus();
+            } else {
+                this.model.set('cssSelector', cssSelector);
+                this.$(cssSelector).on('inview', _.bind(this.inview, this));
+            }
+        },
+
+        resizeImage: function(width) {
+            var src = this.$('.textPlus-graphic-inner img').attr('data-' + width);
+            this.$('.textPlus-graphic-inner img').attr('src', src);
+            this.$('.textPlus-graphic-inner').imageready(_.bind(function() {
+                this.setReadyStatus();
+            }, this));
+        },
+
+        openPopup: function (event) {
+            event.preventDefault();
+            var outerMargin = parseFloat(this.$('.textPlus-popup-inner').css('margin'));
+            var innerPadding = parseFloat(this.$('.textPlus-popup-inner').css('padding'));
+            var toolBarHeight = this.$('.textPlus-toolbar').height();
+            this.$('.textPlus-popup-content').removeClass('textPlus-hidden');
+            this.$('.textPlus-popup-inner').css('height', $(window).height() - (outerMargin * 2) - (innerPadding * 2));
+            this.$('.textPlus-popup').removeClass('textPlus-hidden');
+            this.$('.textPlus-popup-content').css('height', (this.$('.textPlus-popup-inner').height() - toolBarHeight));
+        },
+
+        closePopup: function (event) {
+            event.preventDefault();
+            this.$('.textPlus-popup-close').blur();
+            this.$('.textPlus-popup-content').addClass('textPlus-hidden');
+            this.$('.textPlus-popup').addClass('textPlus-hidden');
+        },
+
+        inview: function(event, visible, visiblePartX, visiblePartY) {
+            if (visible) {
+                if (visiblePartY === 'top') {
+                    this._isVisibleTop = true;
+                } else if (visiblePartY === 'bottom') {
+                    this._isVisibleBottom = true;
+                } else {
+                    this._isVisibleTop = true;
+                    this._isVisibleBottom = true;
+                }
+
+                if (this._isVisibleTop && this._isVisibleBottom) {                   
+                    this.$(this.model.get('cssSelector')).off('inview');
+                    this.setCompletionStatus();
+                }
+            }
+        }
+        
+    });
+    
+    Adapt.register("textPlus", TextPlus);
+
+    return TextPlus;
+    
+});
